152d78d854528b735323aca392754f964a2e44eb
JapP <buquiranjapjap@gmail.com>
2025-12-04 07:49:11 +0800

    changes

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
 
-    console.log('MongoDB Connected');
+    console.log('MongoDB Connected',mongoose.connection.name);
     
   } catch (error) {
 
